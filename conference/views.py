3e9b094ec3ba1fbe2081930d9634e64db0f0ac52
Hasib Rafi <hasibulislam405@gmail.com>
2021-07-31 04:19:10 +0600

    Basic routing setup

--- conference/views.py
+++ conference/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    context = {
+        'name':'Rafi',
+        'university': 'American International University Bangladesh',
+        'age' : 26,
+        'city': 'Dhaka',
+        'country': 'Bangladesh',
+        'passion':'Data Science',
+    }
+    return render(request, 'index.html', context)
